47a8184f7b22583b2da694eb3f1dae1861d9d61c
Stefc <stefan.boether@gmail.com>
2019-04-07 08:39:42 +0200

    Struktur für ersten Test vorbereitet

---
+++ tests/SetUpBoardTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace minesweeper.tests
+{
+    public class SetUpBoardTest
+    {
+        [Fact]
+        public void SetUpBoardBeginner()
+        {
+
+        }
+    }
+}
