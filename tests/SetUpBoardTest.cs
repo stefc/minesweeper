a31cbd8d9684924fa224c6fed795705d11a0b67f
Stefc <stefan.boether@gmail.com>
2019-04-07 08:45:57 +0200

    Grauer Test für Board SetUp Beginner

--- tests/SetUpBoardTest.cs
+++ tests/SetUpBoardTest.cs
@@ -8,7 +8,11 @@ namespace minesweeper.tests
         [Fact]
         public void SetUpBoardBeginner()
         {
+			var board = BoardFactory.CreateBoard(height: 9, width: 9, mines: 10);
 
+			Assert.Equal(9, board.Height);
+			Assert.Equal(9, board.Width);
+			Assert.Equal(10, board.Mines.Count());
         }
     }
 }
