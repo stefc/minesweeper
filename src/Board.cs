4d39fc8226425e0267bcbfe78f633fd474cd04ba
Stefc <stefan.boether@gmail.com>
2019-04-07 09:12:27 +0200

    Klassen Board und Point sind hinzugekommen

--- src/Board.cs
+++ src/Board.cs
@@ -1,11 +1,41 @@
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using minesweeper.common;
 
 namespace minesweeper {
 
+	public class Point : ValueObject
+	{
+		public int X { get; }
+		public int Y { get; }
+
+		public Point(int x, int y)
+		{
+			this.X = x;
+			this.Y = y;
+		}
+
+		protected override IEnumerable<object> GetAtomicValues()
+		{
+			yield return this.X;
+			yield return this.Y;
+		}
+	}
+
     public class Board
     {
-        internal readonly int Height;
-        internal readonly int Width;
-        internal readonly IEnumerable<object> Mines;
+		public int Height { get; private set; }
+		public int  Width { get; private set; }
+        private readonly ImmutableHashSet<Point> mines;
+
+		public Board(int width, int height, int mines)
+		{
+			this.Width = width;
+			this.Height = height;
+
+			this.mines = ImmutableHashSet<Point>.Empty;
+		}
+
+        public IEnumerable<Point> Mines => this.mines;
     }
 }
--- src/BoardFactory.cs
+++ src/BoardFactory.cs
@@ -3,7 +3,7 @@ namespace minesweeper {
 	public static class BoardFactory {
 
 		public static Board CreateBoard(int height, int width, int mines) {
-			return null;
+			return new Board(height: height, width: width, mines: mines);
 		}
 	}
     
